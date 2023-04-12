d5a0f870b1a5e3e31b2be49dd4ad3cf09224168c
jiho9118 <mexocoxon@gmail.com>
2023-04-12 23:59:24 +0900

    노션 api 화면 렌더링

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,10 @@
-import Head from "next/head";
-import Layout from "../components/layout";
+import ProjectItem from '../projects/project-item';
+import Head from 'next/head';
+import Layout from '../components/layout';
+import { TOKEN, DATABASE_ID } from '../config';
 
-export default function Projects() {
+export default function Projects({ projects }) {
+  console.log(projects);
   return (
     <Layout>
       <Head>
@@ -10,7 +13,45 @@ export default function Projects() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <h1>프로젝트</h1>
+
+      <h1>프로젝트 개수: {projects.results.length}</h1>
+      
+      {projects.results.map((proj) => (
+        <ProjectItem key={proj.id} data={proj} />
+      ))}
     </Layout>
   );
+}
+
+// 빌드시 호출
+export async function getStaticProps(context) {
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [{ property: '이름', derection: 'ascending' }],
+      page_size: 100,
+    }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+  const project = await res.json();
+
+  const projects = project.results.map(
+    (proj) => proj.properties.이름.title[0].plain_text
+  );
+
+  console.log(`프로젝트 이름: ${projects}`);
+
+  return {
+    props: { projects },
+  };
 }
